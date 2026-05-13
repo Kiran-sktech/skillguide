c884b5683f242abc2dfaea68d09870009ad5ef32
kiran <kiransomavanshi22@gmail.com>
2026-05-13 15:13:32 +0530

    first commit - skillguide chatbot

---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const express = require('express')
+const Groq = require('groq-sdk')
+const cors =require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
+
+app.post('/chat', async (req, res) => {
+  const userMessage = req.body.message
+
+  // check if message is empty
+  if (!userMessage) {
+    return res.status(400).json({ error: 'Message is required' })
+  }
+
+  try {
+    const response = await groq.chat.completions.create({
+      model: 'llama-3.3-70b-versatile',
+      messages: [
+        { role: 'user', content: userMessage }
+      ]
+    })
+
+    const reply = response.choices[0].message.content
+    res.json({ reply })
+
+  } catch (error) {
+    console.error('Groq error:', error.message)
+    res.status(500).json({ error: 'Something went wrong. Try again.' })
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
+})
